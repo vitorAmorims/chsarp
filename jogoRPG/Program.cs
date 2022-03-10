07c1c290c34511d3f61985ea8a941b6113789a63
vitorAmorims <vtamorims@gmail.com>
2022-03-10 18:00:27 -0300

    Extraindo metodo

--- jogoRPG/Program.cs
+++ jogoRPG/Program.cs
@@ -10,45 +10,63 @@ namespace jogoRPG
         {
             Arus guerreiroArus = new Arus("Arus", 42, 471, 72);
             guerreiroArus.Info();
-            ArusValidator validator = new ArusValidator();
-            ValidationResult results = validator.Validate(guerreiroArus);
-            Console.WriteLine("Objeto criado com sucesso: "+results.IsValid);
+            ValidarObjArus(guerreiroArus);
+
+            Wedge guerreiroWedge = new Wedge("Wedge", 42, 471, 89);
+            guerreiroWedge.Info();
+            ValidarObjWedge(guerreiroWedge);
+
+            Ienica magaIenica = new Ienica("Ienica", 42, 600, 481);
+            magaIenica.Info();
+            ValidarObjIenica(magaIenica);
+
+            Topapa magoTopapa = new Topapa("Topapa", 42, 120, 620);
+            magoTopapa.Info();
+            ValidarObjTopapa(magoTopapa);
+        }
+        private static void ValidarObjTopapa(Topapa magoTopapa)
+        {
+            TopapaValidator validator = new TopapaValidator();
+            ValidationResult results = validator.Validate(magoTopapa);
+            Console.WriteLine("Objeto criado com sucesso: " + results.IsValid);
             foreach (var item in results.Errors)
             {
                 Console.WriteLine(item.ToString());
             }
-            
-            Wedge guerreiroWedge = new Wedge("Wedge", 42, 471, 89);
-            guerreiroArus.Info();
-            WedgeValidator validator1 = new WedgeValidator();
-            ValidationResult results1 = validator1.Validate(guerreiroWedge);
-            Console.WriteLine("Objeto criado com sucesso: "+results1.IsValid);
-            foreach (var item in results1.Errors)
+        }
+
+        private static void ValidarObjIenica(Ienica magaIenica)
+        {
+            IenicaValidator validator = new IenicaValidator();
+            ValidationResult results = validator.Validate(magaIenica);
+            Console.WriteLine("Objeto criado com sucesso: " + results.IsValid);
+            foreach (var item in results.Errors)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        private static void ValidarObjWedge(Wedge guerreiroWedge)
+        {
+            WedgeValidator validator = new WedgeValidator();
+            ValidationResult results = validator.Validate(guerreiroWedge);
+            Console.WriteLine("Objeto criado com sucesso: " + results.IsValid);
+            foreach (var item in results.Errors)
             {
                 Console.WriteLine(item.ToString());
             }
 
-            Ienica magaIenica = new Ienica("Ienica", 42, 600, 481);
-            magaIenica.Info();
-            IenicaValidator validator2 = new IenicaValidator();
-            ValidationResult results2 = validator2.Validate(magaIenica);
-            Console.WriteLine("Objeto criado com sucesso: "+results1.IsValid);
-            foreach (var item in results2.Errors)
+        }
+
+        private static void ValidarObjArus(Arus guerreiroArus)
+        {
+            ArusValidator validator = new ArusValidator();
+            ValidationResult results = validator.Validate(guerreiroArus);
+            Console.WriteLine("Objeto criado com sucesso: " + results.IsValid);
+            foreach (var item in results.Errors)
             {
                 Console.WriteLine(item.ToString());
             }
-
-            Topapa magoTopapa = new Topapa("Topapa", 42, 120, 620);
-            magoTopapa.Info();
-            TopapaValidator validator3 = new TopapaValidator();
-            ValidationResult results3 = validator3.Validate(magoTopapa);
-            Console.WriteLine("Objeto criado com sucesso: "+results1.IsValid);
-            foreach (var item in results3.Errors)
-            {
-                Console.WriteLine(item.ToString());
-            }
-
-            
         }
     }
 }
