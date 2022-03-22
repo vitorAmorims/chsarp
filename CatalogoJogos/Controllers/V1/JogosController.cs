2ade263ee83dbb3df0b7a8828de90000bcaa380d
vitorAmorims <vtamorims@gmail.com>
2022-03-22 18:17:56 -0300

    add injection service in controller

--- CatalogoJogos/Controllers/V1/JogosController.cs
+++ CatalogoJogos/Controllers/V1/JogosController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CatalogoJogos.ViewModel;
 using CatalogoJogos.InputModel;
+using CatalogoJogos.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogoJogos.Controllers.V1
 {
@@ -12,10 +14,22 @@ namespace CatalogoJogos.Controllers.V1
     [Route("api/v1/[controller]")]
     public class JogosController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<GameViewModel>>> GetGame()
+        private readonly IJogoService _jogoService;
+        public JogosController(IJogoService jogoService)
         {
-            return Ok();
+            _jogoService = jogoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GameViewModel>>> GetGame([FromQuery,
+            Range(1, int.MaxValue)] int page = 1,
+            [FromQuery, Range(1, 50)] int amount = 5)
+        {
+            var result = await _jogoService.GetGames(page, amount);
+
+            if(result.Count() == 0) return NoContent();
+
+            return Ok(result);
         }
 
         [HttpGet("{idGame:guid}")]
