5ba5b2455061c15a88ec3c911080b8271e930863
vitorAmorims <vtamorims@gmail.com>
2022-03-22 17:59:04 -0300

    insert DTO in controllers

--- CatalogoJogos/Controllers/V1/JogosController.cs
+++ CatalogoJogos/Controllers/V1/JogosController.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CatalogoJogos.ViewModel;
+using CatalogoJogos.InputModel;
 
 namespace CatalogoJogos.Controllers.V1
 {
@@ -11,37 +13,37 @@ namespace CatalogoJogos.Controllers.V1
     public class JogosController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<object>>> GetGame()
+        public async Task<ActionResult<List<GameViewModel>>> GetGame()
         {
             return Ok();
         }
 
         [HttpGet("{idGame:guid}")]
-        public async Task<ActionResult<object>> GetGameId(Guid idGame)
+        public async Task<ActionResult<GameViewModel>> GetGameId(Guid idGame)
         {
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<object>> InsertGame([FromBody])
+        public async Task<ActionResult<GameViewModel>> InsertGame(GameInputModel game)
         {
             return Ok();
         }
 
         [HttpPut("{idGame:guid}")]
-        public async ActionResult<object> UpdateGame(Guid idGame, object game)
+        public async Task<ActionResult> UpdateGame(Guid idGame, GameInputModel game)
         {
             return Ok();
         }
 
         [HttpPatch("{idGame:guid}/preco/{price: double}")]
-        public async ActionResult<object> UpdateGame(Guid idGame, double price)
+        public async Task<ActionResult> UpdateGame(Guid idGame, double price)
         {
             return Ok();
         }
 
         [HttpDelete("{idGame:guid}")]
-        public async ActionResult<object> DeleteGame(Guid idGame)
+        public async Task<ActionResult> DeleteGame(Guid idGame)
         {
             return Ok();
         }
