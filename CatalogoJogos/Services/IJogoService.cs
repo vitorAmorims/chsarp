66d30eaa5e3f419d987ec05377490a0b2d68018a
vitorAmorims <vtamorims@gmail.com>
2022-03-22 18:05:16 -0300

    create interface IJogoService

---
+++ CatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CatalogoJogos.InputModel;
+using CatalogoJogos.ViewModel;
+
+namespace CatalogoJogos.Services
+{
+    public interface IJogoService
+    {
+        Task<List<GameViewModel>> GetGames(int page, int amount);
+        Task<GameViewModel> GetGame(Guid id);
+        Task<GameViewModel> InsertGame(GameInputModel game);
+        Task UpdateGame(Guid id, GameInputModel game);
+        Task UpdateGame(Guid id, double Price);
+        Task Remove(Guid id);
+    }
+}
