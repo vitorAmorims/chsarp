9a9af653085d2c4ddfd88e6c0659c9aa8ee4a4b9
vitorAmorims <vtamorims@gmail.com>
2022-03-23 09:53:39 -0300

    create model Game for IGameRepository

---
+++ CatalogoJogos/Model/Game.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CatalogoJogos.Model
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public double Price { get; set; }
+        
+    }
+}
--- CatalogoJogos/Repositories/IGameRepository.cs
+++ CatalogoJogos/Repositories/IGameRepository.cs
@@ -2,16 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CatalogoJogos.InputModel;
+using CatalogoJogos.Model;
 using CatalogoJogos.ViewModel;
 
 namespace CatalogoJogos.Repositories
 {
     public interface IGameRepository
     {
-        Task<List<GameViewModel>> GetGames(int page, int amount);
-        Task<GameViewModel> GetGame(Guid id);
-        Task<GameViewModel> GetGameNameProducer(string name, string producer);
-        Task<GameViewModel> InsertGame(GameInputModel game);
+        Task<List<Game>> GetGames(int page, int amount);
+        Task<Game> GetGame(Guid id);
+        Task<Game> GetGameNameProducer(string name, string producer);
+        Task<Game> InsertGame(GameInputModel game);
         Task UpdateGame(GameInputModel game);
         Task UpdatePriceGame(Guid id, double Price);
         Task Remove(Guid id);
