fbafd98dbede3311abc7fbedc74284e5377e935e
vitorAmorims <vtamorims@gmail.com>
2022-03-23 09:55:38 -0300

    add cotract in JogoService

--- CatalogoJogos/Services/JogoService.cs
+++ CatalogoJogos/Services/JogoService.cs
@@ -1,7 +1,14 @@
+using CatalogoJogos.Repositories;
+
 namespace CatalogoJogos.Services
 {
     public class JogoService
     {
+        private readonly IGameRepository _jogoRepository;
+        public JogoService(IGameRepository jogoRepository)
+        {
+            _jogoRepository = jogoRepository;
+        }
         
     }
 }
