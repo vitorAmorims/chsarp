51465917c8bd82d5b174e258b925bbb54156463b
vitorAmorims <vtamorims@gmail.com>
2022-03-30 09:29:31 -0300

    Exemplo SOLID

--- Order/Program.cs
+++ Order/Program.cs
@@ -6,7 +6,7 @@ namespace Order
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World SOLID c#!");
         }
     }
 }
